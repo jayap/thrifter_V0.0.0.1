11a5cb918528462a4369519af75c9c8abb4e12ae
jayap <jayap@mit.edu>
2013-10-25 15:02:42 -0400

    Init repo with Thrifter_V0001

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThrifterV0001::Application.config.secret_key_base = 'c9986c0a6cc20ecc4874e312a02bded9e1e500cdb574ba27ae6aaa95e7e0d0e8ff3e45f6318a494c26c790a022242b3081c4564c40c5d6c9d1451fc0f6d83e64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThrifterV0001::Application.config.session_store :cookie_store, key: '_thrifter_V0_0_0_1_session'
